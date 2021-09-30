766aed3919cc4f502d0e3071f1daad7597fea0da
Shreyas Mayya <smayya337@gmail.com>
2021-09-30 10:11:55 -0400

    feat(main): add read and info functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+mod parser;
+
+use std::io::stdin;
+
 fn main() {
-    println!("Hello, world!");
+    info();
+    loop {
+        let input = read();
+    }
+}
+
+fn read() -> String {
+    let mut input: String = String::new();
+    stdin().read_line(&mut input)
+        .ok()
+        .expect("Failed to read line");
+    input
+}
+
+fn info() {
+    let name = env!("CARGO_PKG_NAME");
+    let desc = env!("CARGO_PKG_DESCRIPTION");
+    let version = env!("CARGO_PKG_VERSION");
+    let authors = str::replace(env!("CARGO_PKG_AUTHORS"), ":", ", ");
+    let license = env!("CARGO_PKG_LICENSE");
+    let repo = env!("CARGO_PKG_REPOSITORY");
+    println!("{}", name);
+    println!("{}", desc);
+    println!("Version {}", version);
+    println!("By {}", authors);
+    println!("Available at {} under the {} license.", repo, license);
 }
