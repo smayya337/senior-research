0a19ed6fddee36db827dd2fce4421f47801daef3
Shreyas Mayya <smayya337@gmail.com>
2021-10-14 10:15:04 -0400

    feat(parser): test various special characters

--- src/parser.rs
+++ src/parser.rs
@@ -1,8 +1,26 @@
-pub fn separate(input: &str) -> Vec<&str> {
-    let split = input.split_whitespace();
-    let command_parts: Vec<&str> = Vec::new();
-    for s in split {
-        println!("{}", s);
+pub fn separate(input: &str) -> Vec<String> {
+    let mut single_quote: bool = false;
+    let mut double_quote: bool = false;
+    let mut backslash: bool = false;
+    let mut space: bool = false;
+    let mut dollar: bool = false;
+    let mut paren: bool = false;
+    let mut backtick: bool = false;
+    let mut newline: bool = false;
+    let mut command_parts: Vec<String> = Vec::new();
+    for c in input.chars() {
+        println!("{}", c);
+        match c {
+            '\'' => println!("Single quote!"),
+            '"' => println!("Double quote!"),
+            '\\' => println!("Backslash!"),
+            ' ' => println!("Space!"),
+            '$' => println!("Dollar!"),
+            '(' | ')' => println!("Parenthesis!"),
+            '`' => println!("Backtick!"),
+            '\n' => println!("New line!"),
+            _ => println!("Nothing special!")
+        }
     }
     command_parts
 }
