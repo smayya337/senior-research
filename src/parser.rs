ebfad70066539caf2972117a1ddaedd86a9a13fa
Shreyas Mayya <smayya337@gmail.com>
2021-10-19 10:07:20 -0400

    feat(parser): set up basic split-on-space parser

--- src/parser.rs
+++ src/parser.rs
@@ -1,26 +1,9 @@
-pub fn separate(input: &str) -> Vec<String> {
-    let mut single_quote: bool = false;
-    let mut double_quote: bool = false;
-    let mut backslash: bool = false;
-    let mut space: bool = false;
-    let mut dollar: bool = false;
-    let mut paren: bool = false;
-    let mut backtick: bool = false;
-    let mut newline: bool = false;
-    let mut command_parts: Vec<String> = Vec::new();
-    for c in input.chars() {
-        println!("{}", c);
-        match c {
-            '\'' => println!("Single quote!"),
-            '"' => println!("Double quote!"),
-            '\\' => println!("Backslash!"),
-            ' ' => println!("Space!"),
-            '$' => println!("Dollar!"),
-            '(' | ')' => println!("Parenthesis!"),
-            '`' => println!("Backtick!"),
-            '\n' => println!("New line!"),
-            _ => println!("Nothing special!")
-        }
+pub fn separate(input: &str) -> (Option<&str>, Vec<&str>) {
+    let mut split = input.split_whitespace();
+    let mut vec: Vec<&str> = Vec::new();
+    let cmd = split.next();
+    for s in split {
+        vec.push(s);
     }
-    command_parts
+    (cmd, vec)
 }
