d81725458a9b723b24ad496291c4551c8a972986
Shreyas Mayya <smayya337@gmail.com>
2021-11-04 10:08:29 -0400

    refactor: move canonical_path and dotdot to parser

--- src/parser.rs
+++ src/parser.rs
@@ -1,3 +1,5 @@
+use std::path::PathBuf;
+
 pub fn separate(input: &str) -> (Option<&str>, Vec<&str>) {
     let mut split = input.split_whitespace();
     let mut vec: Vec<&str> = Vec::new();
@@ -6,4 +8,30 @@ pub fn separate(input: &str) -> (Option<&str>, Vec<&str>) {
         vec.push(s);
     }
     (cmd, vec)
+}
+
+pub fn canonical_path(x: &&str) -> String {
+    let homedir = home::home_dir().expect("No home dir set!").display().to_string();
+    let mut canon = String::from(*x);
+    if x.starts_with(&"~") {
+        canon = canon.replace("~", &homedir);
+    }
+    else if x.contains(&"..") {
+        let mut spl = x.split("..").peekable();
+        let mut np = Vec::new();
+        while let Some(y) = spl.next() {
+            let p = match spl.peek() {
+                Some(_) => dotdot(PathBuf::from(y)),
+                None => String::from(y),
+            };
+            np.push(p);
+        }
+        canon = np.join("/");
+    }
+    canon
+}
+
+fn dotdot(mut buf: PathBuf) -> String {
+    buf.pop();
+    return buf.display().to_string();
 }
